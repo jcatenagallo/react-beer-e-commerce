ddf9ab3ddfe339a57728accc3ac8f510f0f60db5
Juan Catena <jcatena@litebox.ai>
2024-08-19 09:47:00 -0300

    Add is loading state to /products page

--- src/components/Common/ProductList/ProductList.tsx
+++ src/components/Common/ProductList/ProductList.tsx
@@ -1,8 +1,16 @@
 import tw from 'twin.macro';
+import Skeleton from 'react-loading-skeleton';
 
+import 'react-loading-skeleton/dist/skeleton.css';
 import useGetProducts from '@/hooks/api/useGetProducts';
 
 import ProductCard from '../ProductCard';
+
+const StyledSkeleton = tw(Skeleton)`
+h-[203px]
+w-[155px]
+rounded-2xl
+`;
 
 const StyledWrapper = tw.ul`
 flex
@@ -11,7 +19,17 @@ gap-[13px]
 `;
 
 const ProductList = () => {
-  const { data } = useGetProducts();
+  const { data, isLoading } = useGetProducts();
+
+  if (isLoading) {
+    return (
+      <StyledWrapper>
+        {Array.from({ length: 4 }).map((_, index) => (
+          <StyledSkeleton key={index} />
+        ))}
+      </StyledWrapper>
+    );
+  }
 
   return (
     <StyledWrapper>
